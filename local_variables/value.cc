77a30bb0ef383dcc9bdc869b9bb6faf3225ef5b6
Meng <jenovefa19990227@gmail.com>
2025-10-31 20:50:13 +0800

    reference and address

--- local_variables/value.cc
+++ local_variables/value.cc
@@ -2,15 +2,27 @@
 
 using namespace std;
 
-int func(int y) {                // y will be a copy of the caller's variable x
+int value_func(int y) {                // y will be a copy of the caller's variable x
 	cout << "Address of y is " << &y << endl;
+	y = 1;
 	return y;                    // Copy y into the function's return value
 }
-
+void reference_func(int& y)
+{
+	cout << "Address of y is " << &y << endl;
+	y = 1;
+}
+void address_func(int* y)
+{
+	cout << "Address of y is " << &y << endl;
+	*y = 1;
+}
 int main() {
 	int x = 2;
 	cout << "Address of x is " << &x << endl;
-	int z = func(x);              // z will be a copy of func's return value
-	cout << "Address of z is " << &z << endl;
-	cout << "After calling func(), z = " << z << endl;
+	//int z = value_func(x);
+	reference_func(x);
+	//address_func(&x);
+	//cout << "Address of z is " << &z << endl;
+	cout << "After calling func(), x = " << x << endl;
 }
